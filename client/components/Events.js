590ec34b842a3ec50dce540093db7bc457f0738c
yeb9925 <seungt.han@gmail.com>
2018-03-27 21:46:00 -0400

    Tuning for chenges

--- client/components/Events.js
+++ client/components/Events.js
@@ -9,15 +9,12 @@ export default class Events extends Component {
         </div>
         <div id="my-events-container">
           <div className="events-items">
-            <i className="fas fa-desktop" id="frontend-icon"></i>
             Show 1
           </div>
           <div className="events-items">
-            <i className="fas fa-database"></i>
             Show 2
           </div>
           <div className="events-items">
-            <i className="fas fa-paint-brush"></i>
             Show 3
           </div>
         </div>
