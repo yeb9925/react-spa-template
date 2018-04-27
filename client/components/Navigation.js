667b1abc0c2ad962095d53eef5e8650e499ca768
Tim Han <seungt.han@gmail.com>
2018-04-27 00:05:20 -0400

    Update Navigation.js

--- client/components/Navigation.js
+++ client/components/Navigation.js
@@ -13,11 +13,7 @@ export default class Navigation extends Component {
         <nav id="nav-bar">
           <Link to="welcome-container" activeClass="active" spy={true} smooth={true} className="nav-links">Home</Link>
           <Link to="about-container" activeClass="active" spy={true} smooth={true} className="nav-links">About</Link>
-          <Link to="events-container" activeClass="active" spy={true} smooth={true} className="nav-links">Events</Link>
-          <Link to="adopt-container" activeClass="active" spy={true} smooth={true} className="nav-links">Adoptable Dogs</Link>
-          <Link to="involved-container" activeClass="active" spy={true} smooth={true} className="nav-links">Get Involved</Link>
           <Link to="contact-container" activeClass="active" spy={true} smooth={true} className="nav-links">Contact</Link>
-          <Link to="donate-container" activeClass="active" spy={true} smooth={true} className="nav-links">Donate</Link>
         </nav>
         {
           this.props.children
