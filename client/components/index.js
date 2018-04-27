6b612c7365782c8b264038ad57f5fe61c41a17f0
Tim Han <seungt.han@gmail.com>
2018-04-27 00:02:49 -0400

    Update index.js

--- client/components/index.js
+++ client/components/index.js
@@ -2,8 +2,4 @@ export { default as Navigation } from './Navigation';
 export { default as Main } from './Main';
 export { default as Welcome } from './Welcome';
 export { default as About } from './About';
-export { default as Events } from './Events';
-export { default as AdoptableDogs } from './AdoptableDogs';
-export { default as GetInvolved } from './GetInvolved';
 export { default as Contact } from './Contact';
-export { default as Donate } from './Donate';
