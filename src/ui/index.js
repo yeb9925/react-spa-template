57a4d8f63e49d78d6e0dbdacb86d7acc727e0e8b
Tim Han <seungt.han@gmail.com>
2019-03-26 22:37:35 -0400

    Update index.js

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import Routes from './routes';
-import './index.scss';
+import Routes from './routes'
+import './index.scss'
 
 ReactDOM.render(
   <Routes />,
