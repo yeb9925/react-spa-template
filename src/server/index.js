e8a9ca4a5b51063d3c5aa17ad5785e91c1258212
Tim Han <seungt.han@gmail.com>
2019-03-26 22:39:57 -0400

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const app = express();
-const PORT = 8080;
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const PORT = 8080
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const path = require('path')
 
 // Logging Middleware
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 
 // Body-parsing Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Static-File Serving Middleware
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 // Sends index.html
 app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
 });
 
 // Error Handling
 app.use((err, req, res, next) => {
-  console.error(err);
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error.');
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
 });
 
 app.listen(PORT, () => {
