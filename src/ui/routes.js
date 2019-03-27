4ec11d7221d97a2d5b9a62750afeaa6230f0c1b8
Tim Han <seungt.han@gmail.com>
2019-03-26 22:38:02 -0400

    Update routes.js

--- src/ui/routes.js
+++ src/ui/routes.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Router } from 'react-router';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Router } from 'react-router'
+import { Route, Switch } from 'react-router-dom'
 
 import history from './history'
-import { Navigation, Main } from './components';
+import { Navigation, Main } from './components'
 
 export default class Routes extends Component {
   render() {
