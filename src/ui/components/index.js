ea348e702b2d44f82179d67c8165730edbf847ec
Tim Han <seungt.han@gmail.com>
2019-03-26 22:39:29 -0400

    Update index.js

--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -1,5 +1,5 @@
-export { default as Navigation } from './Navigation';
-export { default as Main } from './Main';
-export { default as Welcome } from './Welcome';
-export { default as About } from './About';
-export { default as Contact } from './Contact';
+export { default as Navigation } from './Navigation'
+export { default as Main } from './Main'
+export { default as Welcome } from './Welcome'
+export { default as About } from './About'
+export { default as Contact } from './Contact'
