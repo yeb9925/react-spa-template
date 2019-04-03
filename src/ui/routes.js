99589fcec8cd3a1152002336f48d3e7f0e4fae2b
Tim Han <seungt.han@gmail.com>
2019-04-03 11:10:03 -0400

    Remove history (#2)

--- src/ui/routes.js
+++ src/ui/routes.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react'
 import { Router } from 'react-router'
 import { Route, Switch } from 'react-router-dom'
 
-import history from './history'
 import { Navigation, Main } from './components'
 
 export default class Routes extends Component {
   render() {
     return (
-      <Router history={history}>
+      <Router>
         <Navigation>
           <Switch>
             <Route exact path="/" component={Main} />
