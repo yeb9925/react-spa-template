c3b4bad7511e968481814546886ed2a31edfb365
Tim Han <seungt.han@gmail.com>
2019-04-03 11:17:11 -0400

    Revert "Remove history (#2)" (#3)
    This reverts commit 99589fcec8cd3a1152002336f48d3e7f0e4fae2b.

--- src/ui/routes.js
+++ src/ui/routes.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { Router } from 'react-router'
 import { Route, Switch } from 'react-router-dom'
 
+import history from './history'
 import { Navigation, Main } from './components'
 
 export default class Routes extends Component {
   render() {
     return (
-      <Router>
+      <Router history={history}>
         <Navigation>
           <Switch>
             <Route exact path="/" component={Main} />
