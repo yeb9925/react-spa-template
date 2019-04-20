405dcbd0e2f60217e3d314d24873e48d7a0de7d1
yeb9925 <seungt.han@gmail.com>
2019-04-20 17:18:29 -0400

    Update link text

--- src/ui/components/Navigation.js
+++ src/ui/components/Navigation.js
@@ -10,9 +10,9 @@ export default class Navigation extends Component {
     return (
       <div id="nav-container">
         <nav id="nav-bar">
-          <Link to="component1-container" activeClass="active" spy={true} smooth={true} className="nav-links">Component #1</Link>
-          <Link to="component2-container" activeClass="active" spy={true} smooth={true} className="nav-links">Component #2</Link>
-          <Link to="component3-container" activeClass="active" spy={true} smooth={true} className="nav-links">Component #3</Link>
+          <Link to="component1-container" activeClass="active" spy={true} smooth={true} className="nav-links">Component1</Link>
+          <Link to="component2-container" activeClass="active" spy={true} smooth={true} className="nav-links">Component2</Link>
+          <Link to="component3-container" activeClass="active" spy={true} smooth={true} className="nav-links">Component3</Link>
         </nav>
         {
           this.props.children
