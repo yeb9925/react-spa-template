ad3a431a87e207ee26f663ff52bd3dde24fee597
Tim Han <seungt.han@gmail.com>
2019-04-22 16:24:47 -0400

    Add cofig file

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 8080
+const { PORT } = require('../../config')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const path = require('path')
